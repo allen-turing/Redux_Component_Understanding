407aff8307346ad3d2506d272aa8cb176aa36fd6
Pratyush Kumar Singh <prtsh004@gmail.com>
2021-04-08 17:31:36 +0530

    creating store

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import App from "./App";
+import allReducer from "./reducers";
+import { Provider } from "react-redux";
+
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
